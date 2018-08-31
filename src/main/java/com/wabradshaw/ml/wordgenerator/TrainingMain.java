fbb8a8925c3481dccf5c35438cdfb7d52328440a
William Bradshaw <w.a.bradshaw@gmail.com>
2018-08-31 17:26:06 +0100

    Added stubs for the tokenisers.

--- src/main/java/com/wabradshaw/ml/wordgenerator/TrainingMain.java
+++ src/main/java/com/wabradshaw/ml/wordgenerator/TrainingMain.java
@@ -1,15 +1,27 @@
 package com.wabradshaw.ml.wordgenerator;
 
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
 /**
  * Main which allows you to train a neural network, including generating samples, and saves the output as a
  */
 public class TrainingMain {
-    public static void main( String[] args ) throws Exception {
-        // Create config
-        // Get RNN
+    public static void main(String[] args) throws Exception {
+
+        NetworkConfiguration config = new NetworkConfiguration(Mode.LEXEMES,
+                                                               NetworkConfiguration.CHARS_EN_CAPS_WITH_COMMON,
+                                                               200,
+                                                               1234);
+
+        MultiLayerNetwork network = config.createNetwork();
+
         // Load file
         // Split file
         // Tokenize
+        INDArray tokens = config.getTokeniser().getTokens();
+
+        // Create iterator
         // Train
         // Examples
         // Save Model
---
+++ src/main/java/com/wabradshaw/ml/wordgenerator/tokenisation/Tokeniser.java
@@ -0,0 +1,17 @@
+package com.wabradshaw.ml.wordgenerator.tokenisation;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/**
+ * Interface for classes which convert portions of the word-phoneme data into vector tokens.
+ */
+public interface Tokeniser {
+
+    /**
+     * Main point of tokenisers. Reads in training data and converts each line into an array of character-vectors.
+     *
+     * @return
+     */
+    public INDArray getTokens();
+
+}
