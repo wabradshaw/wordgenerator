96ec1027361efeef4a94ac0a4a496a4ddacfbc5d
William Bradshaw <w.a.bradshaw@gmail.com>
2018-08-31 17:54:19 +0100

    FileTokeniser now loads files into a list of lines.

--- src/main/java/com/wabradshaw/ml/wordgenerator/tokenisation/AbstractFileTokeniser.java
+++ src/main/java/com/wabradshaw/ml/wordgenerator/tokenisation/AbstractFileTokeniser.java
@@ -2,8 +2,16 @@ package com.wabradshaw.ml.wordgenerator.tokenisation;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public abstract class AbstractFileTokeniser implements Tokeniser {
 
+    private static final String FILENAME = "phonemes.txt";
+    private static final int IGNORED_LINES = 126;
     protected final String[] possibleTokens;
 
     public AbstractFileTokeniser(String[] possibleTokens){
@@ -12,8 +20,7 @@ public abstract class AbstractFileTokeniser implements Tokeniser {
 
     @Override
     public INDArray getTokens() {
-        // Load file
-        // Get lines
+        List<String> contents = loadFile();
         // Remove bad lines
 
         // Split according to implementation
@@ -22,5 +29,29 @@ public abstract class AbstractFileTokeniser implements Tokeniser {
         // Vectorise
         // Convert to INDArray
         return null;
+    }
+
+    private List<String> loadFile(){
+        try {
+            List<String> results = new ArrayList<>();
+
+            File file = new File(this.getClass().getResource("/phonemes.txt").getFile());
+            Scanner scanner = new Scanner(file);
+
+            int lineNumber = 0;
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if(lineNumber >= IGNORED_LINES){
+                    results.add(line);
+                }
+                lineNumber++;
+            }
+
+            return results;
+
+        } catch(FileNotFoundException e){
+            throw new RuntimeException("Phonemes file could not be found.", e);
+        }
+
     }
 }
