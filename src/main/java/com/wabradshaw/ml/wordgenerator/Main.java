3faddb24cef9eb872d09143dfcb3f7edb9ce9808
William Bradshaw <w.a.bradshaw@gmail.com>
2018-08-31 15:49:15 +0100

    Stub for the main

---
+++ src/main/java/com/wabradshaw/ml/wordgenerator/Main.java
@@ -0,0 +1,10 @@
+package com.wabradshaw.ml.wordgenerator;
+
+/**
+ * Main entry point allowing you to run the system.
+ */
+public class Main {
+    public static void main( String[] args ) throws Exception {
+        System.out.println("TODO");
+    }
+}
