2252764fd1c7f4048b6ece330f7f5cc1080d5ac1
William Bradshaw <w.a.bradshaw@gmail.com>
2018-09-10 02:00:07 +0100

    Added the ability to run on Phonemes rather than Lexemes.

--- src/main/java/com/wabradshaw/ml/wordgenerator/TokenSet.java
+++ src/main/java/com/wabradshaw/ml/wordgenerator/TokenSet.java
@@ -4,17 +4,20 @@ import com.wabradshaw.ml.wordgenerator.tokenisation.LexemeTokeniser;
 import com.wabradshaw.ml.wordgenerator.tokenisation.PhonemeTokeniser;
 import com.wabradshaw.ml.wordgenerator.tokenisation.Tokeniser;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public enum TokenSet {
-    CHARS_EN_CAPS(Mode.LEXEMES,
+    CHARS_EN_CAPS(x -> new LexemeTokeniser(x),
         "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"),
 
-    CHARS_EN_CAPS_WITH_COMMON(Mode.LEXEMES,
+    CHARS_EN_CAPS_WITH_COMMON(x -> new LexemeTokeniser(x),
         "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,-,'"),
 
-    PHONEMES_ARPABET_SEPARATE_STRESSES(Mode.PHONEMES,
+    PHONEMES_ARPABET_SEPARATE_STRESSES(x -> new PhonemeTokeniser(x, true),
         "AA,AE,AH,AO,AW,AY,B,CH,D,DH,EH,ER,EY,F,G,HH,IH,IY,JH,K,L,M,N,NG,OW,OY,P,R,S,SH,T,TH,UH,UW,V,W,Y,Z,ZH,0,1,2"),
 
-    PHONEMES_ARPABET_STRESSED(Mode.PHONEMES,
+    PHONEMES_ARPABET_STRESSED(x -> new PhonemeTokeniser(x, false),
         "AA,AA0,AA1,AA2,AE,AE0,AE1,AE2,AH,AH0,AH1,AH2,AO,AO0,AO1,AO2,AW,AW0,AW1,AW2,AY,AY0,AY1,AY2,B,CH,D,DH,EH," +
         "EH0,EH1,EH2,ER,ER0,ER1,ER2,EY,EY0,EY1,EY2,F,G,HH,IH,IH0,IH1,IH2,IY,IY0,IY1,IY2,JH,K,L,M,N,NG,OW,OW0,OW1,OW2," +
         "OY,OY0,OY1,OY2,P,R,S,SH,T,TH,UH,UH0,UH1,UH2,UW,UW0,UW1,UW2,V,W,Y,Z,ZH");
@@ -23,15 +26,11 @@ public enum TokenSet {
     public static final String EOF_TOKEN = "$";
 
     private final String[] tokens;
-    private final Mode mode;
+    private final Function<TokenSet, Tokeniser> getTokeniser;
 
-    TokenSet(Mode mode, String tokenString){
-        this.mode = mode;
+    TokenSet(Function<TokenSet, Tokeniser> getTokeniser, String tokenString){
+        this.getTokeniser = getTokeniser;
         this.tokens = (START_TOKEN + "," + EOF_TOKEN + "," + tokenString).split(",");
-    }
-
-    public Mode getMode(){
-        return this.mode;
     }
 
     public String[] getTokens(){
@@ -43,18 +42,11 @@ public enum TokenSet {
     }
 
     /**
-     * Builds the {@link Tokeniser} based on the {@link Mode} of the system.
+     * Builds the {@link Tokeniser} that can split a file into characters for this {@link TokenSet}.
      *
      * @return A {@link Tokeniser}
      */
     public Tokeniser getTokeniser(){
-        switch(mode){
-            case LEXEMES:
-                return new LexemeTokeniser(this);
-            case PHONEMES:
-                return new PhonemeTokeniser(this);
-            default:
-                throw new IllegalArgumentException("Please supply an operation Mode.");
-        }
+       return this.getTokeniser.apply(this);
     }
 }
--- src/main/java/com/wabradshaw/ml/wordgenerator/tokenisation/PhonemeTokeniser.java
+++ src/main/java/com/wabradshaw/ml/wordgenerator/tokenisation/PhonemeTokeniser.java
@@ -2,21 +2,41 @@ package com.wabradshaw.ml.wordgenerator.tokenisation;
 
 import com.wabradshaw.ml.wordgenerator.TokenSet;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PhonemeTokeniser extends AbstractTokeniser {
 
-    public PhonemeTokeniser(TokenSet tokenSet) {
-        super(tokenSet);
-    }
+    private final boolean splitStresses;
 
-    @Override
-    public List<Integer> tokenise(String word) {
-        return null;
+    public PhonemeTokeniser(TokenSet tokenSet, boolean splitStresses) {
+        super(tokenSet);
+        this.splitStresses = splitStresses;
     }
 
     @Override
     public String getRelevantWord(String line) {
-        return null;
+        String[] split = line.split(" ", 2);
+
+        if(split.length == 2){
+            return split[1];
+        } else {
+            // Line is improperly formed and should be ignored.
+            return null;
+        }
+    }
+
+    @Override
+    public List<Integer> tokenise(String word) {
+        if(this.splitStresses){
+            word = word.replaceAll("(\\d)", " $1");
+        }
+
+        String[] symbols = word.split(" ");
+        return Arrays.stream(symbols)
+                     .filter(x -> !x.isEmpty())
+                     .map(this::toToken)
+                     .collect(Collectors.toList());
     }
 }
